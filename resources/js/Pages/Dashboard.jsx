7756d8fcdca78b597ab1d327ba4a2a4d428e2473
akbarsazish <sazishaliakbar@gmail.com>
2024-08-31 08:34:27 +0330

    add react chart

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -2,6 +2,45 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
 export default function Dashboard({ auth }) {
+      
+      const Series = [
+        {
+          label: 'React Charts',
+          data: [
+            {
+              date: new Date(),
+              stars: 202123,
+            }
+            // ...
+          ]
+        },
+        {
+          label: 'React Query',
+          data: [
+            {
+              date: new Date(),
+              stars: 10234230,
+            }
+            // ...
+          ]
+        }
+      ];
+
+      const primaryAxis = React.useMemo(
+        (): AxisOptions<DailyStars> => ({
+          getValue: datum => datum.date,
+        }),
+        []
+      )
+      const secondaryAxes = React.useMemo(
+        (): AxisOptions<DailyStars>[] => [
+          {
+            getValue: datum => datum.stars,
+          },
+        ],
+        []
+      )
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -12,7 +51,15 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                        <Chart
+       options={{
+         data,
+         primaryAxis,
+         secondaryAxes,
+       }}
+     />
+                        </div>
                     </div>
                 </div>
             </div>
