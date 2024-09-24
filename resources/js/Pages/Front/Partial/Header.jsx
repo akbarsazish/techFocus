1b09222cc7e0aa3fa900fc35d64b378dc79f084a
akbarsazish <sazishaliakbar@gmail.com>
2024-09-24 17:49:47 +0330

    add function to toggle the dropdown

--- resources/js/Pages/Front/Partial/Header.jsx
+++ resources/js/Pages/Front/Partial/Header.jsx
@@ -1,6 +1,13 @@
 
+import React, { useState } from 'react';
 
 export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleDropdown = () => {
+        setIsOpen((prev) => !prev);
+    };
+
+
   return (
     <nav className="bg-gray-800">
         <div className="mx-w-full px-2 sm:px-6 lg:px-8">
@@ -19,43 +26,46 @@ export default function Header() {
                         </svg>
                     </button>
                 </div>
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                   <img className="h-8 w-auto" src="/images/techfocus.png" alt="techfocus" />
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                    <div className="flex space-x-4">
-                        <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                        <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white">Team</a>
-                        <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white">Projects</a>
-                        <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white">Calendar</a>
-                        <div className="relative group">
-                            <a 
-                                type="button" 
-                                className="about rounded-md px-3 py-2 text-sm font-medium text-white cursor-pointer inline-block" 
-                                id="menu-button" 
-                                aria-expanded="true" 
-                                aria-haspopup="true"
-                            >
-                           About
-                            <svg className="-mr-1 h-5 w-5 text-gray-400 inline-block" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
-                            </svg>
-                        </a>
-                        <div className="absolute hidden group-hover:block right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
-                            <div className="py-1" role="none">
-                                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="menu-item-0">About Us</a>
-                                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="menu-item-1">Team</a>
-                                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="menu-item-2">Customers</a>
-                                <form method="POST" action="#" role="none">
-                                    <button type="submit" className="block w-full px-4 py-2 text-left text-sm text-gray-700" role="menuitem" tabIndex="-1" id="menu-item-3">Sign out</button>
-                                </form>
+
+                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                    <div className="flex flex-shrink-0 items-center">
+                    <img className="h-8 w-auto" src="/images/techfocus.png" alt="techfocus" />
+                    </div>
+                    <div className="hidden sm:ml-6 sm:block">
+                        <div className="flex space-x-4">
+                            <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
+                            <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white">Team</a>
+                            <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white">Projects</a>
+                            <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white">Calendar</a>
+
+                            <div className="relative inline-block text-left">
+                                <div>
+                                    <button type="button"  onClick={toggleDropdown} className="rounded-md px-3 py-2 text-sm font-medium text-white " id="menu-button" aria-expanded="true" aria-haspopup="true">
+                                    Options
+                                    <svg className="-mr-1 h-5 w-5 text-gray-400 inline-block" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                                    </svg>
+                                    </button>
+                                </div>
+                                {isOpen && (
+                                <div className="absolute left-0 z-10 mt-2 w-40 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                                    <div className="py-1" role="none">
+                                
+                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
+                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
+                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-2">License</a>
+                                    <form method="POST" action="#" role="none">
+                                        <button type="submit" className="block w-full px-4 py-2 text-left text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-3">Sign out</button>
+                                    </form>
+                                    </div>
+                                </div>
+                                )}
+                    
                             </div>
                         </div>
                     </div>
-                    </div>
                 </div>
-            </div>
+
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                     <span className="absolute -inset-1.5"></span>
@@ -64,25 +74,6 @@ export default function Header() {
                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                     </svg>
                 </button>
-
-            
-                <div className="relative ml-3">
-                    <div>
-                        <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                           <span className="absolute -inset-1.5"></span>
-                           <span className="sr-only">Open user menu</span>
-                           <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                        </button>
-                    </div>
-
-                    <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                    
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-                       
-                    </div>
-                </div>
                </div>
             </div>
         </div>
