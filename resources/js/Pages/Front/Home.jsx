661f42a5e780df8b2978cf2240b781326e9c556e
akbarsazish <sazishaliakbar@gmail.com>
2024-09-29 23:03:02 +0330

    Add new style to service

--- resources/js/Pages/Front/Home.jsx
+++ resources/js/Pages/Front/Home.jsx
@@ -8,13 +8,13 @@ export default function Home() {
           <div className="max-w-auto">
             <div className="flex flex-row p-20 text-white bg-[url('../../public/images/banner.jpg')] bg-no-repeat bg-cover bg-center h-screen">
                   <div className="className mt-16">
-                      <h1 className="text-4xl mb-2"> Welcome to TechFocus </h1>
+                      <h1 className="text-[44px] font-bold mb-4"> Welcome to TechFocus </h1>
                       <p className="text-xl">
                           The techFocus, utilizing advanced technology methods and the expertise of its personnel, proudly designs portals and web-based software tailored to the needs of employers.
                           An attractive user interface, high security, appropriate speed, dynamic management, and optimization for search engines are among the key priorities for professionally designing portals and web-based software.
                       </p>
                       <div className="mt-16 text-center">
-                      <button className="bg-transparent hover:bg-gray-800 text-white-700 font-semibold border border-white-500 hover:text-white py-2 px-4 rounded">
+                      <button className="bg-transparent hover:bg-purple-700 text-white-700 font-semibold border border-white-500 hover:text-white py-2 px-4 rounded">
                           Start With Us
                       </button>
                       </div>
@@ -24,17 +24,17 @@ export default function Home() {
                  <div className="mb-12">
                     <h1 className="text-3xl font-bold text-white text-center">Services</h1>
                  </div>
-                <div className="grid grid-cols-4 gap-4 text-center text-gray">
-                    <div className="bg-gray-100 rounded h-56 -mt-10">
+                <div className="grid grid-cols-4 gap-4 text-center text-white">
+                    <div className="bg-indigo-900 rounded h-56 -mt-10 hover:bg-purple-700">
                         <h2 className="font-bold text-2xl mt-5"> Web and Mobile App Development</h2>
                     </div>
-                    <div className="bg-gray-100 rounded h-56">
+                    <div className="bg-indigo-900 rounded h-56 hover:bg-purple-700">
                         <h2 className="font-bold text-2xl mt-5">Hosting </h2>
                     </div>
-                    <div className="bg-gray-100 rounded h-56">
+                    <div className="bg-indigo-900 rounded h-56 hover:bg-purple-700">
                         <h2 className="font-bold text-2xl mt-5">Marketing</h2>
                     </div>
-                    <div className="bg-gray-100 rounded h-56 -mt-10">
+                    <div className="bg-indigo-900 rounded h-56 -mt-10 hover:bg-purple-700">
                         <h2 className="font-bold text-2xl mt-5">SEO</h2>
                     </div>
                 </div>
