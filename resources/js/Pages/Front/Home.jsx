4cee880b681beec2c2f0eb634900e98b7f259840
akbarsazish <sazishaliakbar@gmail.com>
2024-09-29 22:44:56 +0330

    Add services section

--- resources/js/Pages/Front/Home.jsx
+++ resources/js/Pages/Front/Home.jsx
@@ -2,6 +2,45 @@
 import Header from './Partial/Header';
 export default function Home() {    
   return ( 
-    <Header />
+    <>
+      <Header />
+      <div className="dark:bg-gray-900">
+          <div className="max-w-auto">
+            <div className="flex flex-row p-20 text-white bg-[url('../../public/images/banner.jpg')] bg-no-repeat bg-cover bg-center h-screen">
+                  <div className="className mt-16">
+                      <h1 className="text-4xl mb-2"> Welcome to TechFocus </h1>
+                      <p className="text-xl">
+                          The techFocus, utilizing advanced technology methods and the expertise of its personnel, proudly designs portals and web-based software tailored to the needs of employers.
+                          An attractive user interface, high security, appropriate speed, dynamic management, and optimization for search engines are among the key priorities for professionally designing portals and web-based software.
+                      </p>
+                      <div className="mt-16 text-center">
+                      <button className="bg-transparent hover:bg-gray-800 text-white-700 font-semibold border border-white-500 hover:text-white py-2 px-4 rounded">
+                          Start With Us
+                      </button>
+                      </div>
+                  </div>
+            </div>
+            <div className="section p-20">
+                 <div className="mb-12">
+                    <h1 className="text-3xl font-bold text-white text-center">Services</h1>
+                 </div>
+                <div className="grid grid-cols-4 gap-4 text-center text-gray">
+                    <div className="bg-gray-100 rounded h-56 -mt-10">
+                        <h2 className="font-bold text-2xl mt-5"> Web and Mobile App Development</h2>
+                    </div>
+                    <div className="bg-gray-100 rounded h-56">
+                        <h2 className="font-bold text-2xl mt-5">Hosting </h2>
+                    </div>
+                    <div className="bg-gray-100 rounded h-56">
+                        <h2 className="font-bold text-2xl mt-5">Marketing</h2>
+                    </div>
+                    <div className="bg-gray-100 rounded h-56 -mt-10">
+                        <h2 className="font-bold text-2xl mt-5">SEO</h2>
+                    </div>
+                </div>
+            </div>
+          </div>
+      </div>
+    </>
   )
 }
--- resources/js/Pages/Front/Partial/Header.jsx
+++ resources/js/Pages/Front/Partial/Header.jsx
@@ -2,11 +2,11 @@
 import React, { useState } from 'react';
 
 export default function Header() {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleDropdown = () => {
-        setIsOpen((prev) => !prev);
-    };
+    const [openMenu, setOpenMenu] = useState(null); 
 
+    const toggleMenu = (menu) => {
+        setOpenMenu(openMenu === menu ? null : menu);
+    };
 
   return (
     <nav className="bg-gray-800">
@@ -17,12 +17,12 @@ export default function Header() {
                         <span className="absolute -inset-0.5"></span>
                         <span className="sr-only">Open main menu</span>
                         
-                        <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                         </svg>
                         
-                        <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     </button>
                 </div>
@@ -34,34 +34,47 @@ export default function Header() {
                     <div className="hidden sm:ml-6 sm:block">
                         <div className="flex space-x-4">
                             <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                            <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white">Team</a>
-                            <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white">Projects</a>
-                            <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white">Calendar</a>
-
+                           
                             <div className="relative inline-block text-left">
                                 <div>
-                                    <button type="button"  onClick={toggleDropdown} className="rounded-md px-3 py-2 text-sm font-medium text-white " id="menu-button" aria-expanded="true" aria-haspopup="true">
-                                    Options
+                                    <button type="button" onClick={() => toggleMenu('services')} className="rounded-md px-3 py-2 text-sm font-medium text-white " id="menu-button" aria-expanded="true" aria-haspopup="true">
+                                     Services
                                     <svg className="-mr-1 h-5 w-5 text-gray-400 inline-block" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                                        <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
                                     </svg>
                                     </button>
                                 </div>
-                                {isOpen && (
-                                <div className="absolute left-0 z-10 mt-2 w-40 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+
+                                {openMenu === 'services' && (
+                                <div id="servicesItems" className="absolute left-0 z-10 mt-2 w-40 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
                                     <div className="py-1" role="none">
-                                
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-2">License</a>
-                                    <form method="POST" action="#" role="none">
-                                        <button type="submit" className="block w-full px-4 py-2 text-left text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-3">Sign out</button>
-                                    </form>
+                                        <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="menu-item-0"> Web Application </a>
+                                        <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="menu-item-1"> Hosting </a>
+                                        <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="menu-item-2">Marketing</a>
                                     </div>
                                 </div>
                                 )}
-                    
                             </div>
+
+                            <div className="relative inline-block text-left">
+                                <div>
+                                    <button type="button" onClick={() => toggleMenu('contact')}  className="rounded-md px-3 py-2 text-sm font-medium text-white " id="menu-button" aria-expanded="true" aria-haspopup="true">
+                                    Contact Us
+                                    <svg className="-mr-1 h-5 w-5 text-gray-400 inline-block" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
+                                    </svg>
+                                    </button>
+                                </div>
+                            {openMenu === 'contact' && (
+                                <div className="absolute left-0 z-10 mt-2 w-40 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
+                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="menu-item-0"> Support </a>
+                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="menu-item-1"> Work With Us</a>
+                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="menu-item-2">Contact Us</a>
+                                </div>
+                            )}
+                            </div>
+                            <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white">About</a>
+                            <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white">Blog</a>
                         </div>
                     </div>
                 </div>
@@ -70,8 +83,8 @@ export default function Header() {
                 <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                     <span className="absolute -inset-1.5"></span>
                     <span className="sr-only">View notifications</span>
-                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                     </svg>
                 </button>
                </div>
