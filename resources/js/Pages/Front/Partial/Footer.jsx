a5720c504a1224d7c37de45a3c9477b55f20fe71
akbarsazish <sazishaliakbar@gmail.com>
2024-09-30 15:59:13 +0330

    Add social icons

--- resources/js/Pages/Front/Partial/Footer.jsx
+++ resources/js/Pages/Front/Partial/Footer.jsx
@@ -1,3 +1,4 @@
+import {FaFacebook, FaGithub, FaInstagram, FaLinkedinIn, FaTwitter} from 'react-icons/fa';
 
 export default function Footer(){
     return(
@@ -12,6 +13,13 @@ export default function Footer(){
                     </div>
                     <div className="text-center">
                         <h4 className="font-bold text-xl">Contact </h4>
+                        <span className="inline flex flex-row gap-2 mt-5 justify-center">
+                            <a className="bg-gray-800 w-10 h-10 rounded-full flex justify-center items-center cursor-pointer inline hover:bg-purple-700" target="_blank" href="https://www.linkedin.com/"> <FaLinkedinIn /> </a>
+                            <a className="bg-gray-800 w-10 h-10 rounded-full flex justify-center items-center cursor-pointer inline hover:bg-purple-700" target="_blank" href="https://github.com/"> <FaGithub /> </a>
+                            <a className="bg-gray-800 w-10 h-10 rounded-full flex justify-center items-center cursor-pointer inline hover:bg-purple-700" target="_blank" href="https://twitter.com/"> <FaTwitter /> </a>
+                            <a className="bg-gray-800 w-10 h-10 rounded-full flex justify-center items-center cursor-pointer inline hover:bg-purple-700" target="_blank" href="https://www.facebook.com/"> <FaFacebook /> </a>
+                            <a className="bg-gray-800 w-10 h-10 rounded-full flex justify-center items-center cursor-pointer inline hover:bg-purple-700" target="_blank" href="https://www.instagram.com/"> <FaInstagram /> </a>
+                        </span>
                     </div>
                     <div className="text-center">
                         <h4 className="font-bold text-xl">Quick Link</h4>
