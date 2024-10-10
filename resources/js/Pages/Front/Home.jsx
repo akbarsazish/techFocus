3aec56df9b99a594fae55a5f14894fe69c94ea43
akbarsazish <sazishaliakbar@gmail.com>
2024-10-10 16:02:04 +0330

    Push the data

--- resources/js/Pages/Front/Home.jsx
+++ resources/js/Pages/Front/Home.jsx
@@ -3,11 +3,13 @@ import Header from './Partial/Header';
 import Footer from './Partial/Footer';
 import ServicesFeature from './Services/servicesFeature';
 import HostingPrice from './Services/HostinPrice';
+import HeroPart from './Partial/HeroPart';
 
 export default function Home() {    
   return ( 
     <>
       <Header />
+      <HeroPart />
       <ServicesFeature />
       <HostingPrice />
       <Footer/>
--- resources/js/Pages/Front/Partial/Header.jsx
+++ resources/js/Pages/Front/Partial/Header.jsx
@@ -1,5 +1,4 @@
 
-import FrontLayout from '@/Layouts/FrontLayout';
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
@@ -10,7 +9,6 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <FrontLayout>
       <header className="absolute inset-x-0 top-0 z-50">
         <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
           <div className="flex lg:flex-1">
@@ -81,46 +79,5 @@ export default function Header() {
         </Dialog>
       </header>
 
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div aria-hidden="true"
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          />
-        </div>
-        <div className="mx-auto max-w-2xl py-30 sm:py-48 lg:py-50">
-          <div className="text-center">
-            <h1 className="text-balance text-4xl font-bold tracking-tight text-white sm:text-6xl">
-            Code your dreams into reality.
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-white">
-            techFocus designs customized portals and web-based software for employers, prioritizing an attractive user interface, high security, fast performance, dynamic management, and search engine optimization.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a href="#" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Get started
-              </a>
-            </div>
-          </div>
-        </div>
-        <div
-          aria-hidden="true"
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          />
-        </div>
-      </div>
-    </FrontLayout>
   )
 }
