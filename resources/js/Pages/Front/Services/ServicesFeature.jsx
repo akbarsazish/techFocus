3b29dee3d39b575f86851f0c4735ce3be94e201e
akbarsazish <sazishaliakbar@gmail.com>
2024-10-16 17:10:43 +0330

    Refactor: services

--- resources/js/Pages/Front/Services/ServicesFeature.jsx
+++ resources/js/Pages/Front/Services/ServicesFeature.jsx
@@ -41,13 +41,13 @@ export default function ServicesFeature() {
     <FrontLayout>
       
     <div className="py-12 sm:py-22">
-        <div className="max-w-auto">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="section p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12">
                 <div className="mb-12">
                   <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl text-center"> Services </p>
                 </div>
               
-                <div className="grid grid-cols-1 sm:grid-cols-4 gap-2 text-center text-white">
+                <div className="grid grid-cols-1 sm:grid-cols-3 gap-2 text-center text-white">
                     <div className="primary rounded h-56 -mt-10 hover:bg-purple-700">
                         <h2 className="font-bold text-2xl mt-5"> Web and Mobile App Development</h2>
                         <img className="h-auto max-w-full mx-auto p-4" src="/images/web.png" alt="web"/>
@@ -56,10 +56,7 @@ export default function ServicesFeature() {
                         <h2 className="font-bold text-2xl mt-5">Hosting </h2>
                         <img className="h-auto max-w-full mx-auto p-4" src="/images/hosting.png" alt="hosting"/>
                     </div>
-                    <div className="primary rounded h-56 hover:bg-purple-700">
-                        <h2 className="font-bold text-2xl mt-5">Marketing</h2>
-                        <img className="h-auto max-w-full mx-auto p-4" src="/images/marketing.png" alt="marketing"/>
-                    </div>
+                    
                     <div className="primary rounded h-56 -mt-10 mt-0 sm:-mt-10 hover:bg-purple-700">
                         <h2 className="font-bold text-2xl mt-5">SEO</h2>
                         <img className="h-auto max-w-full mx-auto p-4" src="/images/ceo.png" alt="ceo"/>
