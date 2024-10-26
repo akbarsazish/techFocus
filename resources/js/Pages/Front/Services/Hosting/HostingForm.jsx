09b48f06d5f7784662564a94618e27e4668a8761
akbarsazish <sazishaliakbar@gmail.com>
2024-10-26 11:22:39 +0330

    Add some new features

--- resources/js/Pages/Front/Services/Hosting/HostingForm.jsx
+++ resources/js/Pages/Front/Services/Hosting/HostingForm.jsx
@@ -190,17 +190,20 @@ export default function HostingForm() {
                         </div>
                         <div className="flex flex-col md:flex-row mt-4">
                              <div className="w-full md:basis-2/4 px-2">
-                             <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
-                                <TextInput id="bordered-radio-1" type="radio" value="" name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                                <label htmlFor="bordered-radio-1" className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"> Credit Card</label>
-                            </div>
+                                <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+                                    <TextInput id="bordered-radio-1" type="radio" value="" name="bordered-radio" className="w-4 h-4 focus:ring-white dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                    <label htmlFor="bordered-radio-1" className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"> Credit Card</label>
+                                </div>
                              </div>
                              <div className="w-full md:basis-2/4 px-2">
                                 <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
-                                    <TextInput checked id="bordered-radio-2" type="radio" value="" name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                    <TextInput checked id="bordered-radio-2" type="radio" value="" name="bordered-radio" className="w-4 h-4 focus:ring-white dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                                     <label htmlFor="bordered-radio-2" className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">PayPal</label>
                                 </div>
                              </div>
+                        </div>
+                        <div className="flex min-h-32 border-2 my-4 rounded-lg border-dashed border-gray-300">
+
                         </div>
 
                         <div className="mt-4 text-right">
